f7d98f87aba7122b89e104a13e14a89d3ff2ce09
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-12-27 11:59:34 +0000

    states via flask

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def get_states():
+    states_list = []
+    states = storage.all(State)
+    states_list = states.values()
+    return render_template("7-states_list.html", states=states_list)
+
+@app.teardown_appcontext
+def close_session(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')

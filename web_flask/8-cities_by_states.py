ac6e78b622939e010eb83960f3ef2a1bdd1f71d4
pgikonyo <48520811+gisconesheri@users.noreply.github.com>
2023-12-27 13:32:58 +0000

    cities and all states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""serve State objects"""
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/cities_by_states")
+def get_states_and_cities():
+    """serve the state objects from storage"""
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template("8-cities_by_states.html", states=states_list)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """close down current database session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
